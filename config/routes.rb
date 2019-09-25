9477b99c91cb5e2f9ccca14221e2c81c46ea54e4
Muhammad Azmi <muhammad.azmi@ukirama.com>
2019-09-25 15:58:45 +0700

    Finished static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'static_pages#home'
+  get 'about', to: 'static_pages#about'
+  get 'post', to: 'static_pages#post'
+
+  root 'static_pages#home'
+  
 end
