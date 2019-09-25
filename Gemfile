b91aef0fe88a323f5278910caf44a0e713efede6
Muhammad Azmi <muhammad.azmi@ukirama.com>
2019-09-25 16:44:55 +0700

    Added secure password

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem "font-awesome-rails"
 gem "pg"
 gem 'pry'
+gem 'bcrypt'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {maximum:45}
   validates :email, presence: true, length: {maximum:45}
-  
-
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
